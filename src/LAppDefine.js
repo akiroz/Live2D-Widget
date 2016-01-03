22ddb84225321bee0b95ad7e0db1cb9c19b70a99
akiroz <akiroz.vectis@gmail.com>
2016-01-04 02:46:45 +0800

    init

---
+++ src/LAppDefine.js
@@ -0,0 +1,51 @@
+var LAppDefine = {
+    
+    
+    DEBUG_LOG : true,
+    DEBUG_MOUSE_LOG : false, 
+    // DEBUG_DRAW_HIT_AREA : false, 
+    // DEBUG_DRAW_ALPHA_MODEL : false, 
+    
+    
+    
+    
+    VIEW_MAX_SCALE : 2,
+    VIEW_MIN_SCALE : 0.8,
+
+    VIEW_LOGICAL_LEFT : -1,
+    VIEW_LOGICAL_RIGHT : 1,
+
+    VIEW_LOGICAL_MAX_LEFT : -2,
+    VIEW_LOGICAL_MAX_RIGHT : 2,
+    VIEW_LOGICAL_MAX_BOTTOM : -2,
+    VIEW_LOGICAL_MAX_TOP : 2,
+    
+    
+    PRIORITY_NONE : 0,
+    PRIORITY_IDLE : 1,
+    PRIORITY_NORMAL : 2,
+    PRIORITY_FORCE : 3,
+    
+    
+    BACK_IMAGE_NAME : "assets/image/back_class_normal.png",
+
+    
+    MODEL_HARU : "assets/live2d/haru/haru.model.json",
+    MODEL_BLANC : "assets/live2d/blanc/blanc_classic.model.json",
+    MODEL_NEP : "assets/live2d/neptune/neptune_classic.json",
+    MODEL_NOIR : "assets/live2d/noir/noir_classic.json",
+    MODEL_MURAKUMO : "assets/live2d/murakumo/murakumo.model.json",
+    
+    
+    MOTION_GROUP_IDLE : "idle", 
+    MOTION_GROUP_TAP_BODY : "tap_body", 
+    MOTION_GROUP_FLICK_HEAD : "flick_head", 
+    MOTION_GROUP_PINCH_IN : "pinch_in", 
+    MOTION_GROUP_PINCH_OUT : "pinch_out", 
+    MOTION_GROUP_SHAKE : "shake", 
+
+    
+    HIT_AREA_HEAD : "head",
+    HIT_AREA_BODY : "body"
+    
+};
